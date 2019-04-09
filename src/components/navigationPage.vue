77349eab497cf576e339763193dd4dab26e82792
Dawei <15554485117@163.com>
2019-04-09 19:44:45 +0800

    导航配置

--- src/components/navigationPage.vue
+++ src/components/navigationPage.vue
@@ -1,11 +1,11 @@
 <template>
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
-      <el-menu :default-openeds="['features']" unique-opened>
+      <el-menu :default-openeds="['features']" unique-opened @select="handleSelect">
         <el-submenu index="features">
           <template slot="title">
             <i class="el-icon-menu"></i>功能选择
           </template>
-            <el-menu-item index="features-add">添加一个新任务</el-menu-item>
+            <el-menu-item index="features-add" index-path="features-add">添加一个新任务</el-menu-item>
             <el-menu-item index="features-">啊啊大大</el-menu-item>
         </el-submenu>
       </el-menu>
@@ -23,6 +23,11 @@ export default {
     return {
       tableData: Array(10).fill(item)
     };
+  },
+  methods: {
+      handleSelect(key, keyPath) {
+          console.log(key + "  " + keyPath)
+      }
   }
 };
 </script>
